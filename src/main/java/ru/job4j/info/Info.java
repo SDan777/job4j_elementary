8a11784a1c6b80afde8779e00012a646c08e07f4
Данил <sadyrovdanil98@gmail.com>
2023-01-17 23:28:39 +0300

    4.2. Заголовок в файле java [#159036]

--- src/main/java/ru/job4j/info/Info.java
+++ src/main/java/ru/job4j/info/Info.java
@@ -1,2 +1,8 @@
-package ru.job4j.info;public class Info {
+package ru.job4j.info;
+
+public class Info {
+    public static void main(String[] args) {
+        System.out.println("17.01.2023");
+    }
+
 }
--- src/main/java/ru/job4j/info/info.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j.info;
-
-public class Info {
-    public static void main(String[] args) {
-        System.out.println("17.01.2023");
-    }
-
-}
