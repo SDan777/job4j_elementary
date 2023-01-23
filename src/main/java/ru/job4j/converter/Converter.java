38b903c3d4f3ddfdc470b60f6845d260c9911ec7
Данил <sadyrovdanil98@gmail.com>
2023-01-23 22:54:16 +0300

    0. Что такое тестирование [#159431]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -14,5 +14,12 @@ public class Converter {
     public static void main(String[] args) {
         float dollar = Converter.rublesToDollar(180);
         System.out.println("180 rubles are " + dollar + " dollar. ");
+
+        float in = 180;
+        float expended = 3;
+        float out = Converter.rublesToDollar(in);
+        boolean pass = expended == out;
+        System.out.println("180 rubles are 3. Test result : " + pass);
     }
+
 }
