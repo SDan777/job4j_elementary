11781d4c0175c7d7bf2d32d279156ad05b33bf69
Данил <sadyrovdanil98@gmail.com>
2023-01-24 22:50:35 +0300

    1. Модульные тесты. [#159435]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -6,18 +6,18 @@ public class Converter {
        return rsl;
     }
 
-    public static float rublesToDollar(float value) {
+    public static float rubleToDollar(float value) {
         float rsl = value / 60;
         return rsl;
     }
 
     public static void main(String[] args) {
-        float dollar = Converter.rublesToDollar(180);
+        float dollar = Converter.rubleToDollar(180);
         System.out.println("180 rubles are " + dollar + " dollar. ");
 
         float in = 180;
         float expended = 3;
-        float out = Converter.rublesToDollar(in);
+        float out = Converter.rubleToDollar(in);
         boolean pass = expended == out;
         System.out.println("180 rubles are 3. Test result : " + pass);
     }
