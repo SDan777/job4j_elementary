2eb9b947b636cf82428f806bd1107ac945488291
Данил <sadyrovdanil98@gmail.com>
2023-02-23 13:10:48 +0300

    4.1.2. Глупый бот [#31323]

--- src/main/java/ru/job4j/condition/DummyBot.java
+++ src/main/java/ru/job4j/condition/DummyBot.java
@@ -4,9 +4,9 @@ public class DummyBot {
     public static String answer(String question) {
         String rsl = "I don't know. Please, ask another question.";
         if ("Hi, Bot.".equals(question)) {
-            rsl = new String("Hi, SmartAss");
+            rsl = new String("Hi, SmartAss.");
         } else if ("Bye.".equals(question)) {
-            rsl = new String("See you later");
+            rsl = new String("See you later.");
         }
         return rsl;
     }
@@ -16,7 +16,7 @@ public class DummyBot {
         System.out.println(rsl);
         rsl = DummyBot.answer("Bye.");
         System.out.println(rsl);
-        rsl = DummyBot.answer("What you name");
+        rsl = DummyBot.answer("What you name?");
         System.out.println(rsl);
     }
 }
