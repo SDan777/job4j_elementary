b27b913dff647c079abda0eca447e7c8d7cf8fd4
Данил <sadyrovdanil98@gmail.com>
2023-06-07 21:36:51 +0500

    5.7. Ипотека [#156315  [#156315]]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,10 +3,9 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
-        double debt;
         while (amount > 0) {
-            debt = amount * (percent / 100) + amount;
-            amount =  debt - salary;
+            amount = amount * (percent / 100) + amount;
+            amount =  amount - salary;
             year++;
         }
         return year;
