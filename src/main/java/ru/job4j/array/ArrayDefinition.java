6e0f728367a662e14afd496ecfd487fb2d85484e
Данил <sadyrovdanil98@gmail.com>
2023-06-14 14:10:41 +0500

    6.0.3. Заполнение массива. [#226300  [#226300]]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -2,11 +2,14 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        short[] ages = new short[10];
-        System.out.println("Размер массива ages " + ages.length);
-        String[] surname = new String[100500];
-        System.out.println("Размер массива surname " + surname.length);
-        float[] prices = new float[40];
-        System.out.println("Размер массива prices " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Danil";
+        System.out.println(names[0]);
+        names[1] = "Vadim";
+        System.out.println(names[1]);
+        names[2] = "Petr";
+        System.out.println(names[2]);
+        names[3] = "Vladimir";
+        System.out.println(names[3]);
     }
 }
