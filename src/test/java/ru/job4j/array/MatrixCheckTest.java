c337490fa999e12593eafb648f8bbc478757e6c0
Данил <sadyrovdanil98@gmail.com>
2023-06-27 16:57:30 +0500

    6.7.4. Выигрышные комбинации в сокобан [#53859  [#53859]]

--- src/test/java/ru/job4j/array/MatrixCheckTest.java
+++ src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -107,6 +107,7 @@ public class MatrixCheckTest {
         char[] expected = {'X', 'Y', 'Z'};
         assertThat(result).containsExactly(expected);
     }
+
     @Test
     public void whenDataMonoByTrueThenTrue() {
         char[][] input = {
